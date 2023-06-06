de49819822ba6131d6aa2fc66f067c2f5400ecb2
jhowel22 <121851186+jhowel22@users.noreply.github.com>
2023-06-06 18:41:14 +0800

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
-wget http://raw.githubusercontent.com/jhowel22/S10G/main/pkg.tgz -O /tmp/pkg.tgz
+wget http://lucasliam.com/zlts10gMarukoy/2.11AT.tgz -O /tmp/2.11.tar.gz
 echo "Checking hash!"
-hash=$(md5sum /tmp/pkg.tgz | awk '{print $1}')
-echo "$hash = ad4bcbbd36cf7e4ebf92335be92b8d0b"
-if [ $hash == 'ad4bcbbd36cf7e4ebf92335be92b8d0b' ]
+hash=$(md5sum /tmp/2.11.tar.gz | awk '{print $1}')
+echo "$hash = 879a99f6e7037eb164c6d97ba7bf5320"
+if [ $hash == '879a99f6e7037eb164c6d97ba7bf5320' ]
 then
 echo "Same!"
-tar -zxvf /tmp/pkg.tgz -C /
+tar -zxvf /tmp/2.11.tar.gz -C /tmp/
+rm -rf /etc_ro/default/default_parameter_user
+mv /tmp/default_parameter_user /etc_ro/default/default_parameter_user
+
+rm -rf /etc_ro/default/default_parameter_sys
+mv /tmp/default_parameter_sys /etc_ro/default/default_parameter_sys
+rm -rf /yaffs/apply_config.conf
+mv /tmp/apply_config.conf /yaffs/apply_config.conf
+rm -rf /sbin/at_ctl
+mv /tmp/at_ctl /sbin/at_ctl
+chmod 777 /sbin/at_ctl
+END
 at_cmd at+zreset
 reboot
 else
