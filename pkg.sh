05deb7fe954e0377d9af2839c852f9891ef9c02a
jhowel22 <121851186+jhowel22@users.noreply.github.com>
2023-06-06 17:36:06 +0800

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+wget http://github.com/jhowel22/S10G/raw/main/pkg.tgz/pkg.tgz -O /tmp/pkg.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/pkg.tgz | awk '{print $1}')
+echo "$hash = ad4bcbbd36cf7e4ebf92335be92b8d0b"
+if [ $hash == 'ad4bcbbd36cf7e4ebf92335be92b8d0b' ]
+then
+echo "Same!"
+tar -zxvf /tmp/pkg.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
