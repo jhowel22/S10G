f72ad48b04ffc5a16919bf232d6fd2f0ad0f74cb
jhowel22 <121851186+jhowel22@users.noreply.github.com>
2023-06-06 18:27:21 +0800

    Update pkg.sh

--- pkg.sh
+++ pkg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://github.com/jhowel22/S10G/raw/main/pkg.tgz -O /tmp/pkg.tgz
+wget https://raw.githubusercontent.com/jhowel22/S10G/main/pkg.tgz-O /tmp/pkg.tgz
 echo "Checking hash!"
 hash=$(md5sum /tmp/pkg.tgz | awk '{print $1}')
 echo "$hash = ad4bcbbd36cf7e4ebf92335be92b8d0b"
